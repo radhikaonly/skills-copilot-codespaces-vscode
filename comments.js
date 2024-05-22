57bd6718d41f7653baa107351ed5472a9dfa5756
radhikaonly <radhikaonly@hotmail.com>
2024-05-22 20:41:17 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,6 @@
+ // Create web server
+ var http = require('http');
+ var fs = require('fs');
+ var server = http.createServer(function(req, res) {
+     fs.methodOrProperty; // Replace "methodOrProperty" with the appropriate method or property of the fs object
+ });
